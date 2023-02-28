b660237b64f4479cdf446cbca47d72a2ed122b06
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-02-27 20:43:19 -0600

    updated csv

--- StatsExamples/main.py
+++ StatsExamples/main.py
@@ -1,12 +1,20 @@
 from pathlib import Path
 from slippi import *
+import polars as pl
 
-replay = Game(Path(r'Modern Replays/Game_20221227T194333.slp'))
+# replay = Game(Path(r'Modern Replays/Game_20221227T194333.slp'))
 
-frames = []
-flags = []
-for frame in replay.frames:
-    frames.append(frame.ports[0].leader)
-    flags.append(frame.ports[0].leader.post.flags)
+# frames = []
+# flags = []
+# for frame in replay.frames:
+#     frames.append(frame.ports[0].leader)
+#     flags.append(frame.ports[0].leader.post.flags)
 
-print("done")
+# print("done")
+
+data = pl.read_csv(r"StatsExamples\SSBM Data Sheet (1.02) - Character Attributes.csv")
+
+thing = data.filter(pl.col("name") == "Falco")
+
+print(thing)
+print(thing.get_column("gravity"))
